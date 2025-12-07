e52d3cdf0db94635d90b252f8af294026163aebd
Achaz91 <ngwouanza@gmail.com>
2025-12-07 14:39:25 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,37 @@
-
 const CACHE_NAME = "ofc-starter-v1";
-const ASSETS = ["index.html","manifest.json","service-worker.js","icons/icon-192.png","icons/icon-512.png"];
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./products/man_shoes.webp",
+  "./products/woman_shoes.jpeg",
+  "./products/man_bag.webp",
+  "./products/man_perfum.webp",
+  "./products/woman_bag.webp",
+  "./products/woman_parfum.webp"
+];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
